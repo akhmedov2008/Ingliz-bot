1f9c689b27755e6d69813589298e1f948345a972
Mysterious & Programmer <96759283+akhmedov2008@users.noreply.github.com>
2022-01-03 18:02:09 +0300

    Update Admins.py

--- handlers/users/Admins.py
+++ handlers/users/Admins.py
@@ -7,5 +7,4 @@ from keyboards.default.menu import asosiy_menu
 async def bot_start(message: types.Message):
     await message.delete()
     await message.answer(f"Salom {message.from_user.full_name}\n Adminlar:\n \n```````````````````\n \n <a href='https://t.me/Mx_2589/'>Dasturchi</a>: Muhammadamin Ahmedov\n Va Fayzullo Asqarov\n \n```````````````````\n",disable_web_page_preview=True)
-    await message.delete()
 
