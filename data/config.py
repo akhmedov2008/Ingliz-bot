35deae717965f35099371c1887ee0de8973dd7da
akhmedov2008 <96759283+akhmedov2008@users.noreply.github.com>
2022-01-03 15:44:11 +0300

    Create config.py

---
+++ data/config.py
@@ -0,0 +1,6 @@
+# BOT_TOKEN = "1919645958:AAFInNBebHBgcsiFvZXnrowrGlFp15KnhC0"  # Bot token FayzulloTestbot
+# BOT_TOKEN = "2084548211:AAEFqA4bg-G92IWBQfPQFCsgi_TsxmGkiZ4"  # Bot token Namoz_vaqtlari_aniq_bot
+BOT_TOKEN = "1845111770:AAH1lwPC-1ukyIx5I4pMxpDn-x019I_fzRU"  # Bot  token Paxtaobod_IT_center_bot
+
+ADMINS = [1913708962]  # adminlar ro'yxati
+# ADMINS = [1482132517]  # adminlar ro'yxati
