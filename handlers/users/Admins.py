3b41325ff1809bba9f0f51ad217bc995f4bf3da8
akhmedov2008 <96759283+akhmedov2008@users.noreply.github.com>
2022-01-03 15:49:28 +0300

    summaryy

---
+++ handlers/users/Admins.py
@@ -0,0 +1,11 @@
+from aiogram import types
+from loader import dp
+from aiogram.dispatcher.filters import Command
+from keyboards.default.menu import asosiy_menu
+
+@dp.message_handler(Command("Dasturchilar", prefixes="!/"))
+async def bot_start(message: types.Message):
+    await message.delete()
+    await message.answer(f"Salom {message.from_user.full_name}\n Adminlar:\n \n```````````````````\n \n <a href='https://t.me/Mx_2589/'>Dasturchi</a>: Muhammadamin Ahmedov\n Va Fayzullo Asqarov\n \n```````````````````\n",disable_web_page_preview=True)
+    await message.delete()
+
