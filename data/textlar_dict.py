fd79eedf37a4ec4a39010d9915126c398b594607
akhmedov2008 <96759283+akhmedov2008@users.noreply.github.com>
2022-01-03 15:44:48 +0300

    Create textlar_dict.py

---
+++ data/textlar_dict.py
@@ -0,0 +1,10 @@
+
+kurslar_yozilish_javoblari = {
+    "📝Kompyuter savodxonligi":"Paxtaobod IT Markazi 📝Kompyuter savodxonligi kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝Frontend":"Paxtaobod IT Markazi 📝Frontend kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝Backend":"Paxtaobod IT Markazi 📝Backend kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝Grafik dizayn":"Paxtaobod IT Markazi 📝Grafik dizayn kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝IT English":"Paxtaobod IT Markazi 📝IT English kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝One Million Uzbek Coders":"Paxtaobod IT Markazi 📝One Million Uzbek Coders kursiga yozilish uchun ism va familiyangizni yozing",
+    "📝Raqamlashtirish va IT tadbirkorlik":"Paxtaobod IT Markazi 📝Raqamlashtirish va IT tadbirkorlik kursiga yozilish uchun ism va familiyangizni yozing"
+}
