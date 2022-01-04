7324430f13180d09d1f092ff86a4a8c80a289be1
Mysterious & Programmer <96759283+akhmedov2008@users.noreply.github.com>
2022-01-04 09:30:10 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -2,5 +2,5 @@
 # BOT_TOKEN = "2084548211:AAEFqA4bg-G92IWBQfPQFCsgi_TsxmGkiZ4"  # Bot token Namoz_vaqtlari_aniq_bot
 BOT_TOKEN = "1845111770:AAH1lwPC-1ukyIx5I4pMxpDn-x019I_fzRU"  # Bot  token Paxtaobod_IT_center_bot
 
-ADMINS = [1913708962]  # adminlar ro'yxati
+ADMINS = [1913708962,368959240]  # adminlar ro'yxati
 # ADMINS = [1482132517]  # adminlar ro'yxati
